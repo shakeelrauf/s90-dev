f0e2c3e02139184bc993c454475117c0c1ecbbd9
Shakeel rauf <shkeelrauf@gmail.com>
2018-10-09 20:02:56 +0500

    Validation error fixes.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,3 +1,4 @@
+
 class RegistrationsController < ApplicationController
 	def create
 	  if params[:person][:type] == "artist"
@@ -8,15 +9,15 @@ class RegistrationsController < ApplicationController
 		@p = Person::Person.new(email: params[:person][:email],first_name: params[:person][:fname], last_name: params[:person][:lname])
 	  end
 	  if params[:person][:pw] == params[:person][:pw_confirmation]
-		if (params[:person][:pw].present?) and (params[:person][:pw].length == 6)
-		  @p.pw  = @p.encrypt_pw(params[:person][:pw])
-		  if @p.save
-			redirect_to home_path
-	      else
-		    flash[:error] = @p.errors.messages
-			redirect_to sec_signup_path
-		  end
-		else
+			if (params[:person][:pw].present?) and (params[:person][:pw].length >= 6)
+	  		@p.pw  = @p.encrypt_pw(params[:person][:pw])
+			  if @p.save
+					redirect_to home_path
+		    else
+			    flash[:error] = @p.errors.messages
+					redirect_to sec_signup_path
+			  end
+			else
 	  	  flash[:error] = "Password and Confirm Password Must be present OR greater than 6."
 	  	  redirect_to sec_signup_path
 	    end
