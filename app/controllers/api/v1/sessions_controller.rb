5999c9c6982b9f42249f188a36e9cd62cf9a3f4e
Shakeel rauf <shkeelrauf@gmail.com>
2018-10-09 16:57:04 +0500

    Login api added.

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::SessionsController < ApiController
+
+  def create
+    return render_json_response({:msg => "incomplete params", :success => false}, :ok) if !params[:person][:password].present? and !params[:person][:email].present?
+    @p =  Person::Person.where(email: params[:person][:email]).first
+    if @p.present?
+      if @p.encrypt_pw(params[:person][:password]) == @p.pw
+        return render_json_response({:auth_token => @p.authentication_token, :success => true, msg: "successfull Login"}, :ok)
+      else
+        return render_json_response({:msg => "Password doesn't matched", :success => false}, :ok)
+      end
+    else
+      return render_json_response({:msg => "User not found", :success => false}, :ok)
+    end
+  end
+end
