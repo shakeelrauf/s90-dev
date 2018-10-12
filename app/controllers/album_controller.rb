2e79fbe22c7bb5fbc1cd508c910e56c2121f32ca
Shakeel Rauf <shkeelrauf@gmail.com>
2018-10-12 19:48:15 +0500

    Add validation on edit page.

--- app/controllers/album_controller.rb
+++ app/controllers/album_controller.rb
@@ -36,6 +36,9 @@ class AlbumController < ApplicationController
     album.year = params[:field_year].to_i
     album.name = params[:field_name]
     album.save!
+    respond_to do |format|
+      format.html { return redirect_to artists_path }
+    end
     respond_ok
   end
 
