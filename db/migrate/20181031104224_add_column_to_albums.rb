8e9047fbfc56564ff3cddf034e628b900e2716ca
Shakeel Rauf <shkeelrauf@gmail.com>
2018-12-10 17:22:27 +0500

    Change migration with new names.

--- db/migrate/20181031104224_add_column_to_albums.rb
+++ db/migrate/20181031104224_add_column_to_albums.rb
@@ -1,6 +1,6 @@
 class AddColumnToAlbums < ActiveRecord::Migration[5.0]
   def change
     add_column :albums, :artist_id, :integer
-    add_column :songs, :artist_id, :integer
+    add_column :song_songs, :artist_id, :integer
   end
 end
--- db/migrate/20181102055616_change_nullable_values_of_songs.rb
+++ db/migrate/20181102055616_change_nullable_values_of_songs.rb
@@ -1,5 +1,5 @@
 class ChangeNullableValuesOfSongs < ActiveRecord::Migration[5.0]
   def change
-  	change_column :songs, :artist_id, :integer, null: false
+  	change_column :song_songs, :artist_id, :integer, null: false
   end
 end
