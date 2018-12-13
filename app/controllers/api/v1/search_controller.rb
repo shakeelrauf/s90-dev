66ea334cf0805d40b5d9db9f3839f143222e3c33
Shakeel Rauf <shkeelrauf@gmail.com>
2018-12-13 17:28:36 +0500

    Add id in response for identifying

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -26,16 +26,16 @@ class Api::V1::SearchController < ApiController
     indices.each do |si|
       # Artists
       if (si.artist.present?)
-        h = {"label"=>si.l, "pic"=>"", "artist_id"=>si.artist.id.to_s, "res_type"=>"a"}
+        h = {"label"=>si.l,"id"=>si.artist.id, "pic"=>"", "artist_id"=>si.artist.id.to_s, "res_type"=>"a"}
         h["pic"] = si.artist.profile_pic_url if (si.artist.profile_pic_name.present?)
         sects["artists"] << h
       elsif (si.album.present?)
-        h = {"label"=>si.l, "pic"=>"", "album_id"=>si.album.id.to_s,
+        h = {"label"=>si.l,"id"=>si.album.id, "pic"=>"", "album_id"=>si.album.id.to_s,
              "res_type"=>"al", "artist_id"=>si.album.artist.id.to_s}
         h["pic"] = si.album.cover_pic_url
         sects["albums"] << h
       elsif (si.song.present?)  
-        h = {"title"=>si.l, "pic"=>"", "song_id"=>si.song.id.to_s,
+        h = {"title"=>si.l,"id"=>si.song.id, "pic"=>"", "song_id"=>si.song.id.to_s,
              "res_type"=>"s", "artist_id"=>si.song.artist.id.to_s}
         h["pic"] = si.song.album.cover_pic_url if si.song.album.present?
         h["album_name"] = si.song.album.name if si.song.album.present?
