f9fb234fceef6e8c05e752ef7e4526dac82df77d
Shakeel Rauf <shkeelrauf@gmail.com>
2018-12-13 19:26:00 +0500

    Add controllers  for apis and view.

---
+++ app/controllers/api/v1/playlist/playlist_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::Playlist::PlaylistController < ApiController
+	before_action :authenticate_user
+
+	def create
+		return render_json_response({:msg => MISSING_PARAMS_MSG, :success => false}, :ok) if params[:title].nil? || params[:subtitle].nil?
+		pl = Song::Playlist.new()
+		pl.title, pl.subtitle = params[:title], params[:subtitle]
+		pl.person = current_user
+		pl.save!
+		return render_json_response({:playlist => pl , :success => true, msg: SUCCESS_DEFAULT_MSG }, :ok)
+	end
+
+	def all
+		return render_json_response({:playlists => current_user.playlists , :success => true, msg: SUCCESS_DEFAULT_MSG }, :ok)
+	end
+end
