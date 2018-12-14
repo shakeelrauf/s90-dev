019d63f501da2029f950249cd3195bd9ebf67839
Shakeel Rauf <shkeelrauf@gmail.com>
2018-12-14 18:55:53 +0500

    Parse playlist.

--- app/controllers/api/v1/artist/artists_controller.rb
+++ app/controllers/api/v1/artist/artists_controller.rb
@@ -6,10 +6,8 @@ class Api::V1::Artist::ArtistsController < ApiController
 		return render_json_response({:msg => MISSING_PARAMS_MSG, :success => false}, :ok) if params[:artist_id].nil?
 		p = Person::Artist.where(id: params[:artist_id]).first
 		return render_json_response({:msg => USER_NOT_FOUND_MSG, :success => false}, :ok) if p.nil?
- 		
-
 		data = {
-			 playlists: p.playlists, albums: parse_albums(Album::Album.all), songs: parse_songs(p.songs)
+			 playlists: parse_playlists(p.playlists), albums: parse_albums(Album::Album.all), songs: parse_songs(p.songs)
 		}
 		return render_json_response({:msg => SUCCESS_DEFAULT_MSG, :success => true, data:  data}, :ok)
 	end
@@ -18,17 +16,27 @@ class Api::V1::Artist::ArtistsController < ApiController
 		albums_a = []
 		albums.each do |al|
  			album  = JSON.parse(al.to_json)
-			album["cover_pic_url"] = al.cover_pic_url
+			album["pic"] = al.cover_pic_url
 			albums_a.push(album)
  		end
  		return albums_a
+	end
+
+	def parse_playlists(playlists)
+		playlists_a = []
+		playlists.each do |pl|
+ 			playlist  = JSON.parse(pl.to_json)
+			playlist["pic"] = pl.image_url
+			playlists_a.push(playlist)
+ 		end
+ 		return playlists_a
 	end
 
 	def parse_songs(songs)
 		songs_a = []
 		songs.each do |s|
  			song  = JSON.parse(s.to_json)
-			song["cover_pic_url"] = s.album.cover_pic_url if s.album.present?
+			song["pic"] = s.album.cover_pic_url if s.album.present?
 			songs_a.push(song)
  		end
  		return songs_a
