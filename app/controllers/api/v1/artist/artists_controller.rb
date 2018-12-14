4c37cbec82dcadbaf2271fdaeadc32013943a5a7
Shakeel Rauf <shkeelrauf@gmail.com>
2018-12-14 17:06:38 +0500

    Response changes api.

--- app/controllers/api/v1/artist/artists_controller.rb
+++ app/controllers/api/v1/artist/artists_controller.rb
@@ -6,9 +6,31 @@ class Api::V1::Artist::ArtistsController < ApiController
 		return render_json_response({:msg => MISSING_PARAMS_MSG, :success => false}, :ok) if params[:artist_id].nil?
 		p = Person::Artist.where(id: params[:artist_id]).first
 		return render_json_response({:msg => USER_NOT_FOUND_MSG, :success => false}, :ok) if p.nil?
+ 		
+
 		data = {
-			 playlists: p.playlists, albums: p.albums, songs: p.songs
+			 playlists: p.playlists, albums: parse_albums(Album::Album.all), songs: parse_songs(p.songs)
 		}
 		return render_json_response({:msg => SUCCESS_DEFAULT_MSG, :success => true, data:  data}, :ok)
+	end
+
+	def parse_albums(albums)
+		albums_a = []
+		albums.each do |al|
+ 			album  = JSON.parse(al.to_json)
+			album["cover_pic_url"] = al.cover_pic_url
+			albums_a.push(album)
+ 		end
+ 		return albums_a
+	end
+
+	def parse_songs(songs)
+		songs_a = []
+		songs.each do |s|
+ 			song  = JSON.parse(s.to_json)
+			song["cover_pic_url"] = s.cover_pic_url
+			songs_a.push(song)
+ 		end
+ 		return songs_a
 	end
 end
