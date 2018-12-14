fa08d5f72aa4fe8f1446b0b2e8e02fe7d225c7a5
Shakeel Rauf <shkeelrauf@gmail.com>
2018-12-14 13:11:08 +0500

    Response wrap in data variable.

--- app/controllers/api/v1/artist/artists_controller.rb
+++ app/controllers/api/v1/artist/artists_controller.rb
@@ -6,6 +6,9 @@ class Api::V1::Artist::ArtistsController < ApiController
 		return render_json_response({:msg => MISSING_PARAMS_MSG, :success => false}, :ok) if params[:artist_id].nil?
 		p = Person::Artist.where(id: params[:artist_id]).first
 		return render_json_response({:msg => USER_NOT_FOUND_MSG, :success => false}, :ok) if p.nil?
-		return render_json_response({:msg => SUCCESS_DEFAULT_MSG, :success => true, playlists: p.playlists, albums: p.albums, songs: p.songs}, :ok)
+		data = {
+			 playlists: p.playlists, albums: p.albums, songs: p.songs
+		}
+		return render_json_response({:msg => SUCCESS_DEFAULT_MSG, :success => true, data:  data}, :ok)
 	end
 end
