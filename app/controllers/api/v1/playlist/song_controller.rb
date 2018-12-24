b2871a16d0bf109eb20a48bf4686c57c1e0b878a
Shakeel Rauf <shkeelrauf@gmail.com>
2018-12-24 18:31:23 +0500

    Like dislike fix for ids not exist.

--- app/controllers/api/v1/playlist/song_controller.rb
+++ app/controllers/api/v1/playlist/song_controller.rb
@@ -3,12 +3,16 @@ class Api::V1::Playlist::SongController < ApiController
 
 	def like
 		return render_json_response({:msg => MISSING_PARAMS_MSG, :success => false}, :ok) if params[:song_id].nil? 
-		current_user.liked_song_ids = current_user.liked_song_ids << params[:song_id]
+		@song =  Song::Song.find_by_id(params[:song_id])
+		return render_json_response({:msg => NOT_FOUND_DATA_MSG, :success => false}, :ok) if @song.nil?
+   	current_user.liked_song_ids = current_user.liked_song_ids << params[:song_id]
 		return render_json_response({:msg => SUCCESS_DEFAULT_MSG, songs: current_user.liked_songs, :success => false}, :ok)
 	end
 
 	def dislike
 		return render_json_response({:msg => MISSING_PARAMS_MSG, :success => false}, :ok) if params[:song_id].nil? 
+		@song =  Song::Song.find_by_id(params[:song_id])
+		return render_json_response({:msg => NOT_FOUND_DATA_MSG, :success => false}, :ok) if @song.nil?
 		current_user.liked_song_ids = current_user.liked_song_ids - [params[:song_id].to_i]
 		return render_json_response({:msg => SUCCESS_DEFAULT_MSG, songs: current_user.liked_songs, :success => false}, :ok)
 	end
