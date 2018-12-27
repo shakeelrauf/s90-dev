4f42fe093bfb212b126339c5169c3935b92f1bae
Shakeel Rauf <shkeelrauf@gmail.com>
2018-12-27 11:58:02 +0500

    Redem response change.

--- app/controllers/api/v1/store_controller.rb
+++ app/controllers/api/v1/store_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::StoreController < ApiController
   def redeem
     return render_json_response({:msg => MISSING_PARAMS_MSG, :success => false}, :ok) if params[:token].nil?
     code = Store::Code.find_by_token(params[:token])
-    return render_json_response({:msg => ALREADY_REDEEMED_TOKEN_MSG, :success => false}, :ok) if code.nil? || code.redeemed?
+    return render_json_response({:msg => ALREADY_REDEEMED_TOKEN_MSG, :success => true}, :ok) if code.nil? || code.redeemed?
     code.redeemed = true
     code.save!
     return render_json_response({:code => code , :success => true, msg: SUCCESS_DEFAULT_MSG }, :ok)
