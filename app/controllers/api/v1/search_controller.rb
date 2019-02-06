2de876046222f61e919dc4b8362d8f00e1f231a8
Shakeel Rauf <shkeelrauf@gmail.com>
2019-02-06 17:07:42 +0500

    Artist image url.

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -27,7 +27,7 @@ class Api::V1::SearchController < ApiController
       # Artists
       if (si.artist.present?)
         h = {"name"=>si.l,"id"=>si.artist.id, "pic"=>"", "artist_id"=>si.artist.id.to_s, "res_type"=>"a"}
-        h["pic"] = si.artist.profile_pic_url if (si.artist.profile_pic_name.present?)
+        h["pic"] = si.artist.default_image.image_url if (si.artist.images.present?)
         sects["artists"] << h
       elsif (si.album.present?)
         h = {"label"=>si.l,"id"=>si.album.id, "pic"=>"", "album_id"=>si.album.id.to_s,
