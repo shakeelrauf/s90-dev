7b25a2b7a69e56a11433e3b87be28a3ed5dc8ffe
Shakeel Rauf <shkeelrauf@gmail.com>
2019-02-07 12:29:13 +0500

    Correcting route.

--- config/routes.rb
+++ config/routes.rb
@@ -239,7 +239,7 @@ Rails.application.routes.draw do
         end
       end
 
-      resources :albums, only: [] do
+      resources :albums, only: [:index] do
         collection do
           post :show_al
         end
