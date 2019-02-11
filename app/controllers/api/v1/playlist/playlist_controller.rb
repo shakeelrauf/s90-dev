d2cc9a4b4ea0dbab7467069fd913690b3a4f3364
Shakeel Rauf <shkeelrauf@gmail.com>
2019-02-11 19:03:52 +0500

    Fixing.

--- app/controllers/api/v1/playlist/playlist_controller.rb
+++ app/controllers/api/v1/playlist/playlist_controller.rb
@@ -15,7 +15,7 @@ class Api::V1::Playlist::PlaylistController < ApiController
 		return render_json_response({:msg => MISSING_PARAMS_MSG, :success => false}, :ok) if params[:playlist_id].nil?
 		pl = Song::Playlist.find_by_id(params[:playlist_id])
 		return render_json_response({:msg => "Playlist not found", :success => false}, :ok) if pl.nil?
-		songs  = Api::V1::Parser.parse_songs pl.songs
+		songs  = Api::V1::Parser.parse_songs pl.songs, current_user
 		return render_json_response({:songs => songs , :success => true, msg: SUCCESS_DEFAULT_MSG }, :ok)
 	end
 
