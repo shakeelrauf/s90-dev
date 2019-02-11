68dfbfc1d589dbe562f66b1cdab3f98b6666b4a0
Shakeel Rauf <shkeelrauf@gmail.com>
2019-02-11 17:52:51 +0500

    Likes .

--- app/models/concerns/likeable.rb
+++ app/models/concerns/likeable.rb
@@ -5,16 +5,16 @@ module Likeable
   included do
     has_many :likes, as: :likeable, dependent: :destroy
   end
-
-  TYPES_OF_LIKES.each do |r|
-    define_method "liked_#{r}" do
-      model = ARTIST if r == "artists"
-      model = ALBUM if r == "albums"
-      model = PERSON if r == "persons"
-      model = SONG if r == "songs"
-      model = PLAYLIST if r == "playlists"
-      likes = liked_model(model)
-      likes
-    end
-  end
+  #
+  # TYPES_OF_LIKES.each do |r|
+  #   define_method "liked_#{r}" do
+  #     model = ARTIST if r == "artists"
+  #     model = ALBUM if r == "albums"
+  #     model = PERSON if r == "persons"
+  #     model = SONG if r == "songs"
+  #     model = PLAYLIST if r == "playlists"
+  #     likes = liked_model(model)
+  #     likes
+  #   end
+  # end
 end
