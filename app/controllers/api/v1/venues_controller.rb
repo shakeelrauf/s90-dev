11d20ccc5cea29a225f509fc32179159878b9245
Shakeel Rauf <shkeelrauf@gmail.com>
2019-02-18 13:42:19 +0500

    Add tours select2.

--- app/controllers/api/v1/venues_controller.rb
+++ app/controllers/api/v1/venues_controller.rb
@@ -7,4 +7,24 @@ class Api::V1::VenuesController < ApiController
     render_json_response({:data => @venue.flatten, :success => true}, :ok)
   end
 
+  def index
+    search  = "%#{params[:search]}%"
+    returned = {
+        results: [],
+        pagination: {
+            more: false
+        }
+    }
+    venues  =  Venue.where('name LIKE ? OR address  LIKE ? OR city LIKE ?  OR postal_code  LIKE ? OR country LIKE ? OR state LIKE ?', search, search, search, search, search, search)
+    venues.each do |venue|
+      t = ""
+      t += venue.name + ", " if venue.name && venue.name.present?
+      t += venue.city + ", " if venue.city && venue.city.present?
+      t += venue.state + ", " if venue.state && venue.state.present?
+      t += venue.postal_code  if venue.postal_code && venue.postal_code.present?
+      returned[:results].push({id: venue.id, text: t})
+    end
+    respond_json(returned)
+  end
+
 end
