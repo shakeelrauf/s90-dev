74cc6c33b3e0b9e88213fd64738d81b8ebbc69c8
Shakeel Rauf <shkeelrauf@gmail.com>
2019-02-20 12:30:57 +0500

    Fix all layout home.

--- app/controllers/client/dashboard_controller.rb
+++ app/controllers/client/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class Client::DashboardController < ClientController
-
+	layout 'home'
   def dashboard
   	current_user = Person::Person.last
   	@new_artists = Api::V1::Parser.parse_artists(Person::Artist.where(is_suspended: false).order('created_at DESC').limit(5), current_user)
