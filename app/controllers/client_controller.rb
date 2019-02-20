f31002bf2733e362a71acb08d5e9340595694db1
Hamza Satti <hamza.satti@phaedrasolutions.com>
2019-02-20 16:08:40 +0500

    Changes for current user added

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -2,6 +2,9 @@ class ClientController  < ActionController::Base
 	include Client
 	include SessionRole
 	include ApplicationHelper
+  include AuthenticationResponses
+
+  helper_method :authenticate_user
 	
 	layout "client"
 
@@ -9,4 +12,16 @@ class ClientController  < ActionController::Base
 		render json: resource.to_json, status: status, adapter: :json_api
 	end
 	
+  def current_user
+  	@current_user = Person::Person.find session[:user]["oid"].to_i if session[:user].present?
+  	return @current_user if @current_user
+    return redirect_to "/client/login" if (params[:return_url].nil?)
+  end
+
+  def authenticate_user
+  	debugger
+    if !current_user.present? || current_user == false
+      return render_json_response({:msg => "authenticate_user", :success => false}, :ok) 
+    end
+  end
 end
