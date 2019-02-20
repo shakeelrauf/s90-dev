2b094ecf2361099c089a1140b2c9ba4051aa0fe2
Talha Waseem <talha.waseem@phaedrasolutions.com>
2019-02-20 18:10:57 +0500

    near events backend settings

--- app/controllers/client/dashboard_controller.rb
+++ app/controllers/client/dashboard_controller.rb
@@ -6,9 +6,8 @@ class Client::DashboardController < ClientController
   	@new_artists = Api::V1::Parser.parse_artists(Person::Artist.where(is_suspended: false).order('created_at DESC').limit(5), current_user)
    	@recently_played = Api::V1::Parser.parse_songs(Song::Song.order('last_played DESC').limit(5),current_user)
    	@most_played = Api::V1::Parser.parse_songs(Song::Song.order('played_count DESC').limit(10),current_user)
-    venue_points = Venue.joins(:tours).where.not(lat: nil, lng: nil).closest(origin: [cookies[:lat],cookies[:lng]]).uniq.limit(2)
-    # venue_points = Venue.joins(:tours).where.not(lat: nil, lng: nil).closest(origin: [cookies[:lat],cookies[:lng]]).uniq.limit(2)
-    @venues = Api::V1::Parser.venue_parser(venue_points).flatten
+    venue_points = Venue.joins(:tours).where.not(lat: nil, lng: nil).closest(origin: [cookies[:lat],cookies[:lng]]).uniq
+    @venues = Api::V1::Parser.venue_parser(venue_points).flatten.uniq
   end
 
   def search
