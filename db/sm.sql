c722bf78e92bd7787d1d46a08f4b6437a435e990
Patrice Gagnon <patrice@tenaya.ca>
2019-02-25 17:55:54 -0500

    patches for the RoR that wouldn't migrate from scratch.

---
+++ db/sm.sql
@@ -0,0 +1,8 @@
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `schema_migrations` (
+  `version` varchar(255) NOT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
