9cb77f5ba1626d510d0b2c5e7c4f01dfa36b1569
Talha Waseem <talha.waseem@phaedrasolutions.com>
2019-03-01 13:34:41 +0500

    fixes

--- app/controllers/api/v1/venues_controller.rb
+++ app/controllers/api/v1/venues_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::VenuesController < ApiController
 
   def nearest_venues
     @venue = Venue.joins(:tours).where.not(lat: nil, lng: nil).within(100, units: :kms, origin: [params[:lat],params[:lng]]).uniq.limit(2)
-    @venue = Api::V1::Parser.venue_parser(@venue)
+    @venue = Api::V1::Parser.venue_parser(@venue, current_user)
     # @venue = Venue.closest(origin: [params[:lat],params[:lng]]).limit(2)
     render_json_response({:data => @venue.flatten, :success => true}, :ok)
   end
