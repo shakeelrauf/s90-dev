2f9c3a8fbfc09d8b313c6eb0595bb8492f84d9ef
Talha Waseem <talha.waseem@phaedrasolutions.com>
2019-03-04 17:27:01 +0500

    ujs fix

--- app/assets/javascripts/client.js
+++ app/assets/javascripts/client.js
@@ -10,5 +10,4 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery_ujs
 //= require_tree ./client
