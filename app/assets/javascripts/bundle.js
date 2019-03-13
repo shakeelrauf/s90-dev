9003f2fb5f093ecd15ca090f5d93a8722334ba03
shakeel rauf <shakeel@phaedrasolutions.com>
2019-03-13 15:47:44 +0500

    Fix popper.js.map error.

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -7708,7 +7708,6 @@ Popper.placements = placements;
 Popper.Defaults = Defaults;
 
 /* harmony default export */ __webpack_exports__["default"] = (Popper);
-//# sourceMappingURL=popper.js.map
 
 /* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./../../../webpack/buildin/global.js */ "../node_modules/webpack/buildin/global.js")))
 
