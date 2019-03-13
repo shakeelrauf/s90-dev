6bb0a74bdc1ed5b6f79aab0b889ec996129af28f
shakeel rauf <shakeel@phaedrasolutions.com>
2019-03-13 15:29:46 +0500

    Add QR code to release.

--- app/controllers/concerns/api/v1/album_methods.rb
+++ app/controllers/concerns/api/v1/album_methods.rb
@@ -5,7 +5,7 @@ module Api::V1::AlbumMethods
     return render_json_response({:msg => MISSING_PARAMS_MSG, :success => false}, :ok) if params[:album_id].nil?
     al = Album::Album.not_suspended.find_by_id(params[:album_id])
     if al.present?
-      return render_json_response({:msg => FOUND_DATA_MSG,songs: Api::V1::Parser.parse_songs(al.songs,current_user), :success => true}, :ok)
+      return render_json_response({:msg => FOUND_DATA_MSG, album: Api::V1::Parser.parse_albums(al, current_user),songs: Api::V1::Parser.parse_songs(al.songs,current_user), :success => true}, :ok)
     else
       return render_json_response({:msg => NOT_FOUND_DATA_MSG, :success => false}, :ok)
     end
