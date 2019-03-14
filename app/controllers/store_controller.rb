53115142a8c9505c1d98ded459af8e1616c8494c
Patrice Gagnon <patrice@tenaya.ca>
2019-03-14 18:12:42 -0400

    Code now specific to a release.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,7 +2,7 @@ class StoreController < AdminController
 
 	# Code generation prototype for an artist
 	def create_qr
-		@q = Store::Code.create(:artist_id=>Person::Artist.first.id)	
+		@q = Store::Code.create(:album_id=>Album::Album.last.id)	
 		redirect_to codes_path
 	end
 
