6165c9d956ae59224b5ba1a8e06bc4a9f44c37cc
shakeel rauf <shakeel@phaedrasolutions.com>
2019-03-19 11:20:18 +0500

    Locale and style issues.

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -7,8 +7,11 @@ class ClientController  < ActionController::Base
   include ApplicationHelper
   include Role
 
+  before_action PreFilter
+
   helper_method :authenticate_user
 	before_action :recent_songs
+
 	layout "client"
 
 	def render_json_response(resource, status)
