20ba9726448acecf7a12a34e9f3527a1ce84465b
shakeel rauf <shakeel@phaedrasolutions.com>
2019-03-22 19:01:52 +0500

    Profile pic add in update.

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -40,6 +40,7 @@ class Api::V1::RegistrationsController < ApiController
       return render_json_response({:success => false, msg: PASSWORD_VALIDATION_MSG }, :ok)  if !check_password
       @p.pw =  @p.encrypt_pw(params[:person][:password]) 
     end
+    update_profile_pic(params[:person][:profile_pic]) if params[:person][:profile_pic].present?
     return render_json_response({:auth_token => @p.authenticated,user: Api::V1::Parser.parse_artists(@p, @p), :success => true, msg: REGISTRATION_SUCCESS_MSG}, :ok) if @p.save
     return render_json_response({:errors => @p.errors, :success => false, msg: REGISTRATION_FAIL_MSG}, :ok)
   end
@@ -54,9 +55,38 @@ class Api::V1::RegistrationsController < ApiController
     (params[:person][:password].present? && (params[:person][:password].length>=5) && (params[:person][:password_confirmation]==params[:person][:password]))
   end
 
+  def update_profile_pic(base64)
+    @p = current_user
+    puts "========> #{@p.name}"
+    profile_path = "#{Rails.application.root.to_s}/tmp/profile_pics#{@p.id}"
+    aws_region = ENV['AWS_REGION']
+    FileUtils.mkdir_p profile_path
+    s3 = Aws::S3::Resource.new(region:aws_region)
+    img = nil
+    @img = @p.images.build
+    @img.save
+    fn = "image#{@img.id}.png"
+    file_name = "#{profile_path}/#{fn}"
+    convert_data_url_to_image( base64,file_name )
+    obj = s3.bucket(ENV['AWS_BUCKET']).object("#{@p.class.name.split("::").first.downcase}/#{@p.id}/#{fn}")
+    obj.upload_file(file_name)
+    @img.image_name = fn
+    @img.save! 
+    @p.make_it_default(@img)
+  end
+  
   def person_params
     params.require(:person).permit(:first_name, :last_name, :email, :gender, :dob)
   end
+
+  def convert_data_url_to_image(data_url, file_name)
+    file_name = "#{file_name}"
+    imageDataString = data_url
+    file = File.open("#{file_name}", "wb") {|f| f.write(Base64.decode64(imageDataString["data:image/png;base64,".length .. -1])
+    )}
+    return file
+  end
+
 
   def check_type
     !PersonConstants::TYPES_OF_PERSON.include?(params[:person][:type])
